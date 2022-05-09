ff412a2baf8bb9baa131bbeaa55758f40eb72e27
annomao <omaoannkerubo@gmail.com>
2022-05-09 19:22:43 +0300

    Completed Assignment

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes,Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
